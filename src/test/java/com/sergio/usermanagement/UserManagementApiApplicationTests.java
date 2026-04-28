9449c971a0adf52a23bc0bb293030d52f0c3d655
sergiogv01 <gvsergio31@gmail.com>
2026-04-28 14:52:55 +0200

    feat: Initial Spring Boot project setup

---
+++ src/test/java/com/sergio/usermanagement/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
