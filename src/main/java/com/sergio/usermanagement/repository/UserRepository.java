ca3e57d369382d4577b6a24ac62e114f095346b4
sergiogv01 <gvsergio31@gmail.com>
2026-05-01 16:40:09 +0200

    refactor: delete @Repository annotation from UserRepository

--- src/main/java/com/sergio/usermanagement/repository/UserRepository.java
+++ src/main/java/com/sergio/usermanagement/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.sergio.usermanagement.repository;
 
 import com.sergio.usermanagement.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
@@ -10,7 +9,6 @@ import java.util.Optional;
  * Interfaz de acceso a datos para la entidad User.
  * Proporciona las operaciones básicas CRUD gracias a heredar de JpaRepository
  */
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
