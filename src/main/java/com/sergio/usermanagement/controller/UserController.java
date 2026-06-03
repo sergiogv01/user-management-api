894056cfeffeeacbc0879a1ceeea70dbc84f3f23
sergiogv01 <gvsergio31@gmail.com>
2026-06-03 13:04:40 +0200

    feat: implement GET, PUT and DELETE by ID endpoints in UserController

--- src/main/java/com/sergio/usermanagement/controller/UserController.java
+++ src/main/java/com/sergio/usermanagement/controller/UserController.java
@@ -49,4 +49,40 @@ public class UserController {
     public User createUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
+
+    /**
+     * Endpoint para obtener un usuario por su id.
+     * Ruta: /api/users/id
+     *
+     * @param id Id del usuario a obtener.
+     * @return Usuario obtenido mediante id.
+     */
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    /**
+     * Endpoint para actualizar un usuario existente.
+     * Ruta: PUT /api/users/id
+     *
+     * @param id Id del usuario a actualizar.
+     * @param user El usuario a actualizar.
+     * @return El usuario actualizado.
+     */
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    /**
+     * Endpoint para eliminar un usuario existente.
+     * Ruta: DELETE /api/users/id
+     *
+     * @param id Id del usuario a eliminar.
+     */
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
