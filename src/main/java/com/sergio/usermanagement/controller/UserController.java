e0eb819f429e2bacb167f260e991918863ee8789
sergiogv01 <gvsergio31@gmail.com>
2026-06-04 14:20:16 +0200

    feat: implement validation handling for POST/PUT endpoints and format 404 Bad Request responses

--- src/main/java/com/sergio/usermanagement/controller/UserController.java
+++ src/main/java/com/sergio/usermanagement/controller/UserController.java
@@ -3,11 +3,15 @@ package com.sergio.usermanagement.controller;
 import com.sergio.usermanagement.exceptions.UserNotFoundException;
 import com.sergio.usermanagement.models.User;
 import com.sergio.usermanagement.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Controlador REST para gestionar las operaciones de usuarios.
@@ -49,7 +53,7 @@ public class UserController {
      */
     @PostMapping
     // @RequestBody indica a Spring que el JSON de la petición debe convertirse en objeto User.
-    public User createUser(@RequestBody User user) {
+    public User createUser(@Valid @RequestBody User user) {
         return userService.saveUser(user);
     }
 
@@ -70,12 +74,12 @@ public class UserController {
      * Endpoint para actualizar un usuario existente.
      * Ruta: PUT /api/users/id
      *
-     * @param id Id del usuario a actualizar.
+     * @param id   Id del usuario a actualizar.
      * @param user El usuario a actualizar.
      * @return El usuario actualizado.
      */
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User user) {
         User updatedUser = userService.updateUser(id, user);
         return ResponseEntity.ok(updatedUser);
     }
@@ -103,5 +107,15 @@ public class UserController {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach((error) ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
